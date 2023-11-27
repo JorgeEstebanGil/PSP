7e17b49447c6556a978bb8721f26431f5dfec565
JorgeEstebanGil <125263700+JorgeEstebanGil@users.noreply.github.com>
2023-11-27 08:35:14 +0100

    Comit

---
+++ T2/ejercicioClase.py
@@ -0,0 +1,4 @@
+#Crea dos procesos, y lístalos usando psutil
+#El primer proceso estará 10 segundos vivo y matará al otro proceso, finalmente creará un fork de si mismo
+#El segundo proceso a los 5 segundos cambiará la prioridad del primer proceso
+
