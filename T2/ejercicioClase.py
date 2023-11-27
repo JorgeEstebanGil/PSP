3cb63cc443dd04ac3310dc579084b8a7a004ba81
JorgeEstebanGil <125263700+JorgeEstebanGil@users.noreply.github.com>
2023-11-27 12:54:01 +0100

    Update ejercicioClase.py

--- T2/ejercicioClase.py
+++ T2/ejercicioClase.py
@@ -1,52 +1,67 @@
-#Crea dos procesos, y lístalos usando psutil
-#El primer proceso estará 10 segundos vivo y matará al otro proceso, finalmente creará un fork de si mismo
-#El segundo proceso a los 5 segundos cambiará la prioridad del primer proceso y lanzará el comando ping a la web de google
-
-import psutil
 import os
 import time
-import signal
 import subprocess
-
 import psutil
-import subprocess
-import os
-import time
-import multiprocessing
+from multiprocessing import Process, Lock
 
 def listar_procesos():
-    print("Lista de procesos:")
-    for proc in psutil.process_iter(['pid', 'name', 'status']):
-        print(f"PID: {proc.info['pid']}, Nombre: {proc.info['name']}, Estado: {proc.info['status']}")
+    print("Listando procesos:")
+    for proceso in psutil.process_iter(['pid', 'name', 'username']):
+        print(proceso.info)
 
-def primer_proceso():
-    print("Proceso 1 iniciado.", os.getpid())
-    time.sleep(10)  # Estará vivo por 10 segundos
-    print("Proceso 1 matando al proceso 2.")
-    if segundo_pid is not None:  # Check if segundo_pid is not None before killing
-        os.kill(segundo_pid, psutil.signal.SIGTERM)  # Matando al segundo proceso
-    print("Proceso 1 creando un fork de sí mismo.")
-    multiprocessing.Process(target=primer_proceso).start()
+def proceso_1(lock, nuevo_proceso_1):
+    pid = os.getpid()
+    print(f"Proceso 1 (PID {pid}) creado.")
 
-def segundo_proceso():
-    print("Proceso 2 iniciado.")
-    time.sleep(5)  # Espera 5 segundos
-    print("Proceso 2 cambiando la prioridad del proceso 1.")
-    proc = psutil.Process(primer_pid)
-    proc.nice(10)  # Cambiando la prioridad del proceso 1
-    print("Proceso 2 lanzando el comando ping a la web de google.")
-    subprocess.run(["ping", "www.google.com"])
+    # Hacer algo durante 10 segundos
+    time.sleep(10)
 
-# Iniciar el primer proceso
-primer_pid = os.getpid()
-segundo_pid = None
+    # Matar al proceso 2
+    with lock:
+        try:
+            nuevo_proceso_1.terminate()
+            print("Proceso 2 terminado.")
+        except AttributeError:
+            print("Proceso 2 no encontrado.")
 
-# Guardar el PID del primer proceso para usarlo en el segundo proceso
-segundo_process = multiprocessing.Process(target=segundo_proceso)
-segundo_process.start()
+    # Crear un nuevo proceso 1
+    print("Creando un nuevo proceso 1...")
+    nuevo_proceso_1 = Process(target=proceso_1, args=(lock, nuevo_proceso_1))
+    nuevo_proceso_1.start()
+    nuevo_proceso_1.join()
 
-# Esperar a que el segundo proceso termine
-segundo_process.join()
+def proceso_2(lock, nuevo_proceso_1):
+    time.sleep(5)
 
-# Listar procesos después de que ambos terminen
-listar_procesos()
+    # Cambiar la prioridad del primer proceso
+    with lock:
+        try:
+            proceso_1_pid = nuevo_proceso_1.pid
+            proceso_1 = psutil.Process(proceso_1_pid)
+            proceso_1.nice(5)  # Cambiar la prioridad a 5
+            print(f"Prioridad del Proceso 1 cambiada a 5.")
+        except psutil.NoSuchProcess:
+            print("Proceso 1 no encontrado.")
+
+    # Lanzar el comando ping a la web de Google
+    print("Lanzando el comando ping a la web de Google...")
+    subprocess.run(["ping", "-c", "5", "www.google.com"])
+
+if name == "main":
+    lock = Lock()  # Crear un objeto 'Lock' para sincronización
+
+    # Crear proceso 1
+    proceso_1_pid = os.getpid()
+    p1 = Process(target=proceso_1, args=(lock, None))
+    p1.start()
+
+    # Crear proceso 2
+    p2 = Process(target=proceso_2, args=(lock, p1))
+    p2.start()
+
+    # Esperar a que ambos procesos terminen
+    p1.join()
+    p2.join()
+
+    # Mostrar información sobre los procesos usando psutil
+    listar_procesos()
